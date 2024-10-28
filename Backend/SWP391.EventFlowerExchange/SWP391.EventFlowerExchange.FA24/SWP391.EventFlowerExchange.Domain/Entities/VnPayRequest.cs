376b935934862f46e43addc752035b423381df47
taikt <taikhuu1102004@gmail.com>
2024-10-28 22:40:20 +0700

    Tai has done tasks
    Product:
    - GetDealProductListBySellerEmail

--- Backend/SWP391.EventFlowerExchange/SWP391.EventFlowerExchange.FA24/SWP391.EventFlowerExchange.Domain/Entities/VnPayRequest.cs
+++ Backend/SWP391.EventFlowerExchange/SWP391.EventFlowerExchange.FA24/SWP391.EventFlowerExchange.Domain/Entities/VnPayRequest.cs
@@ -8,9 +8,9 @@ namespace SWP391.EventFlowerExchange.Domain.Entities
 {
     public class VnPayRequest
     {
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         public double Amount { get; set; }
 
