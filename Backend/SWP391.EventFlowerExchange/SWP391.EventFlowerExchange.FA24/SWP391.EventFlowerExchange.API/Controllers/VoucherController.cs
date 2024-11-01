222ad96b7235e0b37ffd662a5986d00911cce7ff
HuySuper12 <154420124+HuySuper12@users.noreply.github.com>
2024-11-01 21:09:01 +0700

    Huy has done task
    Fix function:
    - GetAllVoucherValid
    - SearchVoucherById
    - SearchVoucherById

--- Backend/SWP391.EventFlowerExchange/SWP391.EventFlowerExchange.FA24/SWP391.EventFlowerExchange.API/Controllers/VoucherController.cs
+++ Backend/SWP391.EventFlowerExchange/SWP391.EventFlowerExchange.FA24/SWP391.EventFlowerExchange.API/Controllers/VoucherController.cs
@@ -24,5 +24,52 @@ namespace SWP391.EventFlowerExchange.API.Controllers
             return Ok(await _service.ViewAllVoucherFromAPIAsync());
         }
 
+        [HttpGet("GetAllVoucherValid")]
+        //[Authorize]
+        public async Task<IActionResult> GetAllVoucherValid(decimal price)
+        {
+            var list = await _service.ViewAllVoucherFromAPIAsync();
+            List<Voucher> listVoucher = new List<Voucher>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (DateTime.Now < list[i].ExpiryDate && price > list[i].MinOrderValue)
+                {
+                    listVoucher.Add(list[i]);
+                }
+            }
+            return Ok(listVoucher);
+        }
+
+        [HttpGet("SearchVoucherById/{id}")]
+        //[Authorize]
+        public async Task<IActionResult> SearchVoucherByIdAsync(string id)
+        {
+            try
+            {
+                var result = await _service.SearchVoucherByIdFromAPIAsync(new Voucher() { VoucherId = int.Parse(id) });
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("VoucherId is a number");
+            }
+
+        }
+
+        [HttpGet("SearchVoucherByCode/{code}")]
+        //[Authorize]
+        public async Task<IActionResult> SearchVoucherByCodeAsync(string code)
+        {
+            var result = await _service.SearchVoucherByCodeFromAPIAsync(code);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound();
+        }
     }
 }
--- Backend/SWP391.EventFlowerExchange/SWP391.EventFlowerExchange.FA24/SWP391.EventFlowerExchange.API/obj/Debug/net8.0/SWP391.EventFlowerExchange.API.AssemblyInfo.cs
+++ Backend/SWP391.EventFlowerExchange/SWP391.EventFlowerExchange.FA24/SWP391.EventFlowerExchange.API/obj/Debug/net8.0/SWP391.EventFlowerExchange.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391.EventFlowerExchange.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d26a3f44a06c31fcaa3cdeb6312c7f15e8d9c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47a760489efeedf7b314ee970413faa053d712e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391.EventFlowerExchange.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391.EventFlowerExchange.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
